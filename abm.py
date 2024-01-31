42be60a8718fde2ac4b0067c9d2b03f127979886
TijnSchickendantz <tijnschickendantz@gmail.com>
2024-01-31 14:24:41 +0100

    initialize

---
+++ abm.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from mesa import Model, Agent
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+import matplotlib.pyplot as plt
+
+
