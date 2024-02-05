d41342b054249f9007b719b6cc4ac867d217df82
TijnSchickendantz <tijnschickendantz@gmail.com>
2024-02-05 16:11:23 +0100

    no switching, random tech + adoption rate

--- abm.py
+++ abm.py
@@ -30,7 +30,7 @@ class Consumer(Agent):
         return random.choice(['brown', 'green'])#self.cons_tech_preference
     
     
-    def cons_payoff(self):
+    def cons_payoff(self, ):
         return 
     def __str__(self):
         return f"Consumer {self.unique_id}"
@@ -54,9 +54,26 @@ class Jurisdiction(Model):
     def __init__(self, n_producers, n_consumers,tax):
 
         self.tax = tax
-        #self.total_brown_products = 0
-        #self.total_green_products = 0
+        
         self.schedule = RandomActivation(self)
+
+        self.n_producers = n_producers
+        self.n_consumers = n_consumers
+
+        self.total_brown_products = 0
+        self.total_green_products = 0
+
+        self.total_brown_producers = 0
+        self.total_green_producers = 0
+
+        self.total_brown_consumers = 0
+        self.total_green_consumers = 0
+
+        self.perc_brown_prod = 0
+        self.perc_green_prod = 0
+
+        self.perc_brown_cons = 0
+        self.perc_green_cons = 0
 
         # Create consumers
         for i in range(n_consumers):
@@ -73,25 +90,18 @@ class Jurisdiction(Model):
         # adoption rate
         # welfare in jurisdiction
         # externalities
-        self.n_producers = n_producers
-        self.n_consumers = n_consumers
-        #self.prop_green = 
 
         self.datacollector = DataCollector(model_reporters={"Total Brown Products": "total_brown_products",
                              "Total Green Products": "total_green_products",
                              "Total Brown Producers": "total_brown_producers",
                              "Total Green Producers": "total_green_producers",
                              "Total Brown Consumers": "total_brown_consumers",
-                             "Total Green Consumers": "total_green_consumers"})
+                             "Total Green Consumers": "total_green_consumers",
+                             "Percentage brown Producers": "perc_brown_prod",
+                             "Percentage green Producers": "perc_green_prod",
+                             "Percentage brown Consumers": "perc_brown_cons",
+                             "Percentage green Consumers": "perc_green_cons"})
         
-        self.total_brown_products = 0
-        self.total_green_products = 0
-
-        self.total_brown_producers = 0
-        self.total_green_producers = 0
-
-        self.total_brown_consumers = 0
-        self.total_green_consumers = 0
 
     def step(self):
 
@@ -109,7 +119,7 @@ class Jurisdiction(Model):
 
         self.total_brown_consumers = 0
         self.total_green_consumers = 0
-        
+
         # Producers produce one product each
         for agent in self.schedule.agents:
             #print(agent)
@@ -121,16 +131,15 @@ class Jurisdiction(Model):
                 elif product_color == 'green':
                     self.total_green_products += 1
                     self.total_green_producers += 1
-        # print(f"Total Brown Products: {self.total_brown_products}")
-        # print(f"Total Green Products: {self.total_green_products}")
-        # print(f"Total Brown Producers: {self.total_brown_producers}")
-        # print(f"Total Green Producers: {self.total_green_producers}")
+        
+        self.perc_brown_prod = self.total_brown_producers / self.n_producers
+        self.perc_green_prod = self.total_green_producers / self.n_producers
 
         # Consumers buy one product each
         for agent in self.schedule.agents:
             if isinstance(agent, Consumer):
                 product_color = agent.buy()
-                print(product_color)
+                #print(product_color)
                 if product_color == 'brown':
                     self.total_brown_consumers += 1
                 if product_color == 'brown' and self.total_brown_products > 0:
@@ -141,14 +150,13 @@ class Jurisdiction(Model):
                     self.total_green_products -= 1
                     #self.total_green_consumers += 1
 
-        # print(f"Total Brown Products: {self.total_brown_products}")
-        # print(f"Total Green Products: {self.total_green_products}")
-
-        # print(f"Total Brown consumers: {self.total_brown_consumers}")
-        # print(f"Total Green consumers: {self.total_green_consumers}")
+        self.perc_brown_cons = self.total_brown_consumers / self.n_consumers
+        self.perc_green_cons = self.total_green_consumers / self.n_consumers
            
         #super().__init__()
 
+
+# RUN MODEL AND PRINT OUTPUTS
 if __name__ == "__main__":
     model = Jurisdiction(n_consumers=10, n_producers=10, tax=1)
     for i in range(5):
@@ -156,6 +164,7 @@ if __name__ == "__main__":
 
     # Retrieve and plot data collected by the DataCollector
     model_data = model.datacollector.get_model_vars_dataframe()
+    #print(model_data)
     # Separate data for brown and green products
     brown_data = model_data.filter(like='Brown')
     green_data = model_data.filter(like='Green')
@@ -164,7 +173,7 @@ if __name__ == "__main__":
     plt.figure(figsize=(10, 5))
 
     # Plot brown products
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     for column in brown_data.columns:
         plt.plot(brown_data[column], label=column)
     plt.title('Brown Products')
@@ -174,7 +183,7 @@ if __name__ == "__main__":
     plt.xticks(range(0, len(brown_data)), map(int, brown_data.index))
 
     # Plot green products
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     for column in green_data.columns:
         plt.plot(green_data[column], label=column)
     plt.title('Green Products')
@@ -183,8 +192,17 @@ if __name__ == "__main__":
     plt.legend()
     plt.xticks(range(0, len(green_data)), map(int, green_data.index))
 
+    # adoption rates
+    plt.subplot(1, 3, 3)
+    #plt.plot(model_data['Percentage brown Producers'], label='Percentage Brown Producers')
+    plt.plot(model_data['Percentage green Producers'], label='Percentage Green Producers')
+    #plt.plot(model_data['Percentage brown Consumers'], label='Percentage Brown Consumers')
+    plt.plot(model_data['Percentage green Consumers'], label='Percentage Green Consumers')
+    plt.title('Adoption of green tech')
+    plt.xlabel('Steps')
+    plt.ylabel('Percentage')
+    plt.legend()
+    plt.xticks(range(0, len(model_data)), map(int, model_data.index))
+
     plt.tight_layout()
     plt.show()
-
-    #for agent in model.schedule.agents:
-    #    print(agent)
